3c913db543880ebbf32fa6d442168f3634e5f4f1
ymateu <devmateu@gmail.com>
2026-09-24 00:21:37 -0300

    install bootstrap and bootstrap-icons

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import './styles.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
